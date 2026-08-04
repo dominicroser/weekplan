6fff006e99f06d4c95094d39b5db035168699e0b
dominicroser <135091554+dominicroser@users.noreply.github.com>
2026-08-04 12:23:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "weekplan-v135";
+const CACHE = "weekplan-v138";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
